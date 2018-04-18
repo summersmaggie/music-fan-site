4cb9d3b109536b2d816ef9f478ee568fd038d631
Kaila Goff and Maggie Summers <maggie@>
2018-04-18 14:49:16 -0700

    add edit album path

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,4 +2,35 @@ class AlbumsController < ApplicationController
   def index
     @albums = Album.all
   end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to  albums_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    @album= Album.find(params[:id])
+    if @album.update(album_params)
+      redirect_to albums_path
+    else
+      render :edit
+    end
+  end
+
+private
+  def album_params
+    params.require(:album).permit(:artist_name, :album_name, :genre)
+  end
 end
